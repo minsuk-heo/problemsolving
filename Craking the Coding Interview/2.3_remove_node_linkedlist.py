f9de7c8692d95f5ff2839fd885b2f5af6c543d2c
Minsuk_Heo <minsuk.heo@gmail.com>
2016-08-03 20:47:01 -0700

    2.3 added

---
+++ Craking the Coding Interview/2.3_remove_node_linkedlist.py
@@ -0,0 +1,59 @@
+__author__ = 'Minsuk Heo'
+"""
+Linked List
+"""
+import unittest
+
+class Node:
+    def __init__(self, item):
+        self.val = item
+        self.next = None
+
+class LinkedList:
+    def __init__(self, item):
+        self.head = Node(item)
+
+    def add(self, item):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(item)
+
+    def remove(self, item):
+        if self.head.val == item:
+            self.head = self.head.next
+        else:
+            cur = self.head
+            while cur.next is not None:
+                if cur.val == item:
+                    self.removeItem(item)
+                    return
+                cur = cur.next
+            print("item does not exist in linked list")
+
+    def removeItem(self, item):
+        cur = self.head
+        while cur.next is not None:
+            if cur.next.val == item:
+                nextnode = cur.next.next
+                cur.next = nextnode
+                break
+
+    def printlist(self):
+        cur = self.head
+        res = []
+        while cur is not None:
+            res.append(cur.val)
+            cur = cur.next
+        return str(res)
+
+
+class LinkedListTest(unittest.TestCase):
+    def test(self):
+        ll = LinkedList(9)
+        ll.add(8)
+        ll.add(7)
+        ll.remove(8)
+
+        self.assertEqual("[9, 7]", ll.printlist())
+
