1716275b3c6d4dc1a6ec97ee46c9408093730b7a
Minsuk_Heo <minsuk.heo@gmail.com>
2016-10-30 15:19:07 -0700

    1.2 other solution added

--- Craking the Coding Interview/1.2_reverseString.py
+++ Craking the Coding Interview/1.2_reverseString.py
@@ -1,9 +1,25 @@
 import unittest
 
+# pythonic way
 def reverseString(str):
     return str[::-1]
+
+# use stack to solve problem
+def reverseString2(str):
+    stack = []
+    for ch in str:
+        stack.append(ch)
+
+    result = ""
+    while len(stack) > 0:
+        result += stack.pop()
+
+    return result
 
 class reverseStringTest(unittest.TestCase):
     def test(self):
         input = "abcde"
         self.assertEquals("edcba", reverseString(input))
+
+        input = "qwert"
+        self.assertEquals("trewq", reverseString2(input))
