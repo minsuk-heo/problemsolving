d383e11bbadedc970fba5cf36f1dcaaeb842f533
Minsuk_Heo <minsuk.heo@gmail.com>
2017-01-01 19:55:01 -0800

    java commit

---
+++ javaSolution/src/data_structure/MyLinkedList.java
@@ -0,0 +1,69 @@
+package data_structure;
+
+/**
+ * Created by Minsuk_Heo on 1/1/2017.
+ */
+public class MyLinkedList {
+    Node head = null;
+
+    public void add(int n){
+        Node newNode = new Node(n);
+        if(head == null){
+            head = newNode;
+        } else{
+            Node cur = head;
+            while(cur.next != null){
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+
+    public void remove(int n){
+        Node cur = head;
+
+        if(head.val == n) {
+            head = cur.next;
+            cur = null;
+        } else {
+            while(cur.next != null) {
+                if(cur.next.val == n) {
+                    cur.next = cur.next.next;
+                    System.out.println(n+ " removed successfully");
+                    return;
+                }
+                cur = cur.next;
+            }
+            System.out.println(n+" is not in linkedlist");
+        }
+
+    }
+
+    public void print(){
+        Node cur = head;
+
+        while(cur != null){
+            System.out.println(cur.val);
+            cur = cur.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList ml = new MyLinkedList();
+        ml.add(1);
+        ml.add(2);
+        ml.add(3);
+        ml.remove(2);
+        ml.print();
+    }
+}
+
+class Node{
+    int val;
+    Node next;
+
+    public Node(int n){
+        val = n;
+        next = null;
+    }
+}
