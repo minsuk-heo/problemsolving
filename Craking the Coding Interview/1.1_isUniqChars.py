172442ae34da45fd17bed5d9dd6229a411d63fe1
Minsuk <minsuk.heo@gmail.com>
2017-06-26 21:08:20 -0700

    Update 1.1_isUniqChars.py

--- Craking the Coding Interview/1.1_isUniqChars.py
+++ Craking the Coding Interview/1.1_isUniqChars.py
@@ -1,6 +1,6 @@
 """
 return true if the string doesn't have duplicate characters
-return false if the string has duplicate character.  
+return false if the string has duplicate character.  updated by other developer
 """
 import unittest
 
